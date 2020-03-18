7d5fc0799aef4ab2b7c75fd0a0e16ecbe5da0ec5
Impactful Actions <eturner.et3@outlook.com>
2020-03-17 20:43:11 -0700

    Bug fixed on frontend where burger names & button text weren't displayed. Bug exists where devour button doesn't change state of burger

---
+++ public/js/burger.js
@@ -0,0 +1,48 @@
+//Hanlders must be attached after DOM is fully loaded 
+
+
+$(function() {
+    //click function for devour button
+    $(".change-devour").on("click", function(event) {
+        var id = $(this).data("id");
+        var newDevour = $(this).data("newdevour");
+
+        var newDevourState = {
+            devour: newDevour
+        };
+
+        //Send updated devour state via put request 
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevourState
+        }).then(
+            function() {
+                console.log(`Change burger state to ${newDevour}`);
+                
+                location.reload();
+            }
+        );
+    });
+});
+
+$(".create-form").on("submit", function(event) {
+    //Have to prevent default on submit event
+    event.preventDefault();
+
+    var newBurger = {
+        name: $("#bu").val().trim()
+    };
+
+    //Send burger via POST request
+    $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+    }).then(
+        function(result) {
+            console.log(`Created a new burger`);
+
+            //Reload the page to view the newly created burger
+            location.reload();
+        }
+    )
+})
